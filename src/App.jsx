de725bc65eded4e02c00e820e9bfe5b40a905da4
Sakhaika <sakha00119@gmail.com>
2026-07-09 18:35:32 +0700

    feat: enhance introduction section in App component with detailed bio and project statistics

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,40 @@ function App() {
 
       {/* Tentang */}
       <div className="tentag mt-32 py-10">
-        <div>
-          <p>Hi, perkenalkan saya Sakha Ibadil Kirom</p>
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            alt="image"
+            className="w-12 rounded-md mb-10 sm:hidden"
+          />
+          <p className="text-base/loose mb-10">
+            Hi, perkenalkan saya Sakha Ibadil Kirom, seorang Full Stack Web
+            Development dan Designer untuk UI/UX maupun Product Digital, saya
+            percaya bahwa desain dan fungsionalitas harus berjalan beriringan,
+            sehingga setipa proyek yang saya kembangakan tidak hanya terlihat
+            menarik tetapi juga memberikan pengalaman pengguna yang optimal.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500"></span>
+                </h1>
+                <p>Proyek Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500"></span>
+                </h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
