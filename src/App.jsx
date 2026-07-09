628b9032a87f556f61ba231024cd3ec05c380407
Sakhaika <sakha00119@gmail.com>
2026-07-09 12:40:08 +0700

    feat: update App and Navbar components with new content and scroll behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 function App() {
   return (
     <>
-      {/* <h1 className="text-4xl text-red-600 font-bold">Sakha Ibadil Kirom</h1> */}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad qui
+        suscipit repellat quidem quis voluptates blanditiis dolor vitae
+        voluptatum dolorem itaque quas earum, quisquam adipisci dignissimos.
+        Obcaecati rem iusto minima vero fuga laudantium voluptatum, voluptates
+        voluptas. Repellat praesentium aspernatur aliquid vero quam, aut animi
+        quos laborum facere asperiores officiis, mollitia voluptas similique!
+        Beatae veniam facilis animi totam! Excepturi suscipit officiis, vel
+        provident, repudiandae et at possimus aspernatur quis cum quod dolorum
+        harum, sapiente quae nemo. Maiores vitae soluta ea illo dolore enim sit
+        accusantium eos delectus rem deserunt dolor harum inventore laborum ut
+        corrupti sunt, omnis nihil cupiditate facere. Explicabo vel voluptatem
+        necessitatibus ducimus at! Voluptatibus earum, temporibus laboriosam
+        alias in beatae cupiditate distinctio saepe quas recusandae voluptates
+        atque dolor sit ea iste voluptas? Autem voluptate nisi in fuga totam eos
+        atque eum excepturi! Reprehenderit cum dicta explicabo, illum eaque
+        commodi eveniet recusandae esse pariatur ullam, laborum nesciunt
+        asperiores totam eius dolores, odit natus atque est quas ab! Saepe iste,
+        qui blanditiis culpa rerum in esse facere quia delectus molestiae earum
+        maiores placeat praesentium dolores sint tempora ipsam voluptatum, odio
+        dignissimos veritatis, et laudantium distinctio illum. Impedit
+        blanditiis inventore odio voluptatum similique accusamus natus? Quos
+        dolorem dignissimos unde consequuntur?
+      </h1>
     </>
   );
 }
