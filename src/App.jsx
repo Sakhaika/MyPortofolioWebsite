7a282fd7903da05537ddbd5e935108e4b0932773
Sakhaika <sakha00119@gmail.com>
2026-07-12 17:26:33 +0700

    feat: update tools section in App component with responsive design and improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -85,11 +85,11 @@ function App() {
 
         <div className="tools mt-32">
           <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
-          <p className="w-2/5 text-base/loose opacity-50">
+          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
             Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan
             Website ataupun Design
           </p>
-          <div className="tools-box mt-14 grid grid-cols-4 gap-4">
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {listTools.map((tool) => (
               <div
                 className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
@@ -101,8 +101,8 @@ function App() {
                   className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
                 />
                 <div>
-                  <h4>{tool.nama}</h4>
-                  <p>{tool.ket}</p>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.ket}</p>
                 </div>
               </div>
             ))}
